7ee91d54f9f9b9ced9dae792bea95a1223d62bd0
ppedziwiatr <ppedziwiatr@gmail.com>
2024-06-01 19:35:27 +0200

    corsssss.....

--- src/app.mjs
+++ src/app.mjs
@@ -5,9 +5,7 @@ import {closePool, createTables} from "./db.mjs";
 import {getLogger} from "./logger.mjs";
 import {THE_BEAVER} from "./beaver.mjs";
 import exitHook from 'async-exit-hook';
-
 dotenv.config();
-// const exitHook = pkg;
 
 const logger = getLogger("server", "trace");
 
@@ -15,23 +13,10 @@ runServer().catch((e) => {
   logger.error(e);
 });
 
-const useCORS = () => {
-  return async (request, response) => {
-    response.header('vary', 'Origin')
-    response.header('Access-Control-Allow-Headers', 'content-type')
-    response.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET')
-    response.header('Access-Control-Allow-Origin', '*')
-    response.header('Access-Control-Allow-Credentials', true)
-  }
-}
-
 async function runServer() {
   await createTables();
   logger.debug("Tables created");
   const webserver = new HyperExpress.Server();
-  webserver.use(useCORS());
-  webserver.options('/*', useCORS());
-
   webserver.use('/', api_v1_router);
   const port = parseInt(process.env.PORT) || 8090;
   await webserver.listen(port);
--- src/router.mjs
+++ src/router.mjs
@@ -3,7 +3,17 @@ import {mainRoute} from "./routes/main.mjs";
 import {resultRoute} from "./routes/result.mjs";
 import {subscribeRoute} from "./routes/sse.mjs";
 
+export const corsMiddleware = async(request, response, next) => {
+  response.header('vary', 'Origin')
+  response.header('Access-Control-Allow-Headers', 'content-type')
+  response.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
+  response.header('Access-Control-Allow-Origin', 'domain')
+  response.header('Access-Control-Allow-Credentials', 'true')
+  next();
+}
+
 export const api_v1_router = new HyperExpress.Router();
 api_v1_router.get('/result/:message-identifier', resultRoute);
-api_v1_router.get('/subscribe/:process-identifier', subscribeRoute);
+api_v1_router.get('/subscribe/:process-identifier', subscribeRoute)
+  .use(corsMiddleware);
 api_v1_router.get('/', mainRoute);
