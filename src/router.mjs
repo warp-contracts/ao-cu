4fe1229a2c4b868e94706fe8287f55d5a693f6ba
ppedziwiatr <ppedziwiatr@gmail.com>
2024-06-01 19:54:14 +0200

    relax

--- src/router.mjs
+++ src/router.mjs
@@ -4,8 +4,6 @@ import {resultRoute} from "./routes/result.mjs";
 import {subscribeRoute} from "./routes/sse.mjs";
 
 export const corsMiddleware = async(request, response, next) => {
-  response.header('vary', 'Origin')
-  response.header('Access-Control-Allow-Headers', 'content-type')
   response.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
   response.header('Access-Control-Allow-Origin', '*')
   response.header('Access-Control-Allow-Credentials', 'true')
